4fe0941a5ca051164e7bd55951719ecff1b20be1
john-roufaeil <john.flouis1@gmail.com>
2024-06-21 21:36:39 +0200

    feat: form input

--- src/app/components/CreatePostBtn.tsx
+++ src/app/components/CreatePostBtn.tsx
@@ -6,13 +6,13 @@ import Link from 'next/link';
 export default function CreatePostBtn() {
     return (
         <Link href={`/create-post`}
-            className="border-grey-50 rounded-lg border-2 w-full lg:w-1/4  h-64 m-4
+            className="border-grey-50 rounded-lg border-2 w-full lg:w-1/4 md:w-1/3 h-64 m-4
         hover:border-primary hover:cursor-pointer transition-colors duration-50
         hover:bg-orange-50  group">
             <div className=' p-4 flex flex-col items-center justify-center border-transparent
              group-hover:border-primary border-2 w-full h-full text-secondary group-hover:text-primary'>
                 <PlusCircleIcon className="w-1/3 mb-2" />
-                <p className="font-semibold text-lg">Create a New Post</p>
+                <p className="font-semibold text-lg text-center">Create a New Post</p>
             </div>
         </Link>
     );
--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -2,8 +2,19 @@
 import Link from 'next/link';
 import { ChevronLeftIcon } from '@heroicons/react/24/outline';
 import { TextField, Button } from '@mui/material';
+import { useState } from 'react';
 
 const CreatePostPage = () => {
+    const [email, setEmail] = useState("");
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    function handleClear() {
+        setEmail("");
+        setTitle("");
+        setBody("");
+    }
+
     return (
         <div className="bg-white w-4/5 mx-auto my-12 p-4 text-black rounded-lg">
             <div className="my-6 flex items-center">
@@ -14,22 +25,38 @@ const CreatePostPage = () => {
                     <h1 className="text-primary text-2xl">Create a New Post</h1>
                 </div>
             </div>
-            <form className='m-12'>
-                <div className='flex justify-between mb-8'>
-                    <TextField id="email-field" label="Email" variant="filled" className='w-5/12' />
-                    <TextField id="title-field" label="Post Title" variant="filled" className='w-6/12' />
+            <form className='m-8'>
+                <div className='flex flex-col md:flex-row md:justify-between mb-8 space-y-8 md:space-y-0'>
+                    <TextField
+                        id="email-field"
+                        label="Email"
+                        variant="filled"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        className='w-full md:w-5/12 my-4 md:m-0'
+                    />
+                    <TextField
+                        id="title-field"
+                        label="Post Title"
+                        variant="filled"
+                        value={title}
+                        onChange={(event) => setTitle(event.target.value)}
+                        className='w-full md:w-6/12 my-4 md:m-0'
+                    />
                 </div>
                 <TextField
-                    className="m-4 w-full"
+                    className="w-full"
                     id="filled-multiline-static"
                     label="Post Body"
                     multiline
                     rows={4}
                     variant="filled"
+                    value={body}
+                    onChange={(event) => setBody(event.target.value)}
                 />
 
-                <div className='flex justify-evenly mt-12'>
-                    <Button variant="outlined" size="large">Clear Form</Button>
+                <div className='flex flex-col md:flex-row space-y-4 md:space-y-0 justify-evenly mt-12'>
+                    <Button variant="outlined" size="large" onClick={() => handleClear()}>Clear Form</Button>
                     <Button variant="contained" size="large">Create Post</Button>
                 </div>
             </form>
