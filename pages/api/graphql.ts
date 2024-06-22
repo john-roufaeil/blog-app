6f34d7a2b735ae5e4435114ebdc03d9b9f49af1e
john-roufaeil <john.flouis1@gmail.com>
2024-06-22 11:59:32 +0200

    feat: apollo works locally from npm run dev

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { ApolloServer } from 'apollo-server-micro';
-// import { MicroRequest } from 'apollo-server-micro/dist/types';
-// import { ServerResponse, IncomingMessage } from 'http';
+import { MicroRequest } from 'apollo-server-micro/dist/types';
+import { ServerResponse, IncomingMessage } from 'http';
 import { typeDefs, resolvers } from '../../graphQL/server';
 
 const apolloServer = new ApolloServer({
@@ -9,18 +9,7 @@ const apolloServer = new ApolloServer({
     resolvers,
 });
 
-// const startServer = apolloServer.start();
-
-// export const config = {
-//     api: {
-//         bodyParser: false,
-//     },
-// };
-
-// export default async function handler(req: MicroRequest, res: ServerResponse<IncomingMessage>) {
-//     await startServer;
-//     await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
-// }
+const startServer = apolloServer.start();
 
 export const config = {
     api: {
@@ -28,4 +17,17 @@ export const config = {
     },
 };
 
-export default apolloServer.createHandler({ path: '/api/graphql' });
+export default async function handler(req: MicroRequest, res: ServerResponse<IncomingMessage>) {
+    await startServer;
+    await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+}
+
+// let serverStart: Promise<void>;
+
+// export default async function handler(res: ServerResponse<IncomingMessage>, req: MicroRequest) {
+//     if (!serverStart) {
+//         serverStart = apolloServer.start();
+//     }
+//     await serverStart;
+//     await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+// }
